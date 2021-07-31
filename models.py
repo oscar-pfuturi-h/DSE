5616589a6aafbe1906f31fde3e17d23e0e4cf326
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-07-31 11:04:53 -0500

    person model

--- models.py
+++ models.py
@@ -2,20 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class EmployeeModel(db.Model):
+class PersonModel(db.Model):
     __tablename__ = "table"
 
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer(),unique = True)
-    name = db.Column(db.String())
+    dni = db.Column(db.Integer(),unique = True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
     age = db.Column(db.Integer())
-    position = db.Column(db.String(80))
+    cellphone = db.Column(db.Integer)
+    address = db.Column(db.String(80))
 
-    def __init__(self, employee_id,name,age,position):
-        self.employee_id = employee_id
-        self.name = name
+    def __init__(self,dni,first_name,last_name,age,cellphone,address):
+        self.dni = dni
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
-        self.position = position
+        self.cellphone = cellphone
+        self.address = address
 
     def __repr__(self):
-        return f"{self.name}:{self.employee_id}"
+        return f"{self.first_name} {self.last_name} | DNI: {self.dni}"
